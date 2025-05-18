c414030b02a8af7416794bfb45735b63a7544670
apoorva110 <apoorvatayal1105@gamil.com>
2025-05-18 13:17:25 +0530

    last commit

---
+++ last_digit_of_number.js
@@ -0,0 +1,6 @@
+//Q - WRITE A PROGRAm IN JAVASCRIPT TO PRINT LAST DIGIT OF THE GIVEN NUMBER
+var a = prompt("Enter any number: ");
+console.log(a);
+var b = a%10;
+console.log("Last digit of the given number: " +b);
+
