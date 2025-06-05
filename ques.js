00f3493dde3bf7d4124da48211f296b90bf46e2d
apoorva110 <apoorvatayal1105@gmail.com>
2025-06-05 09:54:51 +0530

    html_input commit

---
+++ ques.js
@@ -0,0 +1,13 @@
+function handleinput(){
+    let x = document.getElementById('clr').value;
+    console.log(x)
+    document.body.style.backgroundColor=x;
+    document.getElementById("res").innerText=x;
+}
+function handlecalc(){
+    let x=document.getElementById("calc").value;
+    let y=eval(x);
+    document.getElementById("calc").value=y;
+    document.getElementById("res1").innerHTML=y;
+    console.log(eval[x])
+}
