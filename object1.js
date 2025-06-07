75caff2df12a3cebc4b0e4f5fe25470db5a51ed3
apoorva110 <apoorvatayal1105@gmail.com>
2025-06-08 04:01:54 +0530

    object commit

---
+++ object1.js
@@ -0,0 +1,16 @@
+// const product={
+//     name: "parker jotter",
+//     price: 270,
+//     rating: 4.5,
+//     offer:"5%"
+// };
+// console.log(product)
+
+//Q- CREATE A OBJECT CALLED CONST TO STORE INFORMATION GIVEN IN THE PICTURE
+const profile={
+    username:"Shraddha Khapra",
+    followers:345678,
+    following:9607,
+    isFollow:false
+};
+console.log(typeof profile["followers"])
