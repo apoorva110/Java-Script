741036e0269a7a6302ac10a2b954dc6c019ceb4b
apoorva110 <apoorvatayal1105@gmail.com>
2025-06-09 18:01:04 +0530

    js commit

---
+++ practice1.js
@@ -0,0 +1,7 @@
+//Q- GET USER TO INPUT A NUMBER USING PROMPT .CHECK IF THE NUMBER IS A MULTIPLE OF 5 OR NOT
+let a=prompt("Enter any number: ");
+if(a%5==0){
+    alert("It is a multiple of 5")
+}else{
+    alert("It is not a multiple of 5")
+}
